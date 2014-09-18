5523defc57430bf30f00550cd5248428fdebc4da
Titou1384 <titou1384@gmail.com>
2014-09-18 21:21:26 +0200

    v0.0.2 : Implementing some getters & setters in Ticket object

---
+++ class/Ticket.php
@@ -0,0 +1,40 @@
+<?php
+
+class Ticket {
+
+	// Ticket attributes
+	private $type;
+	private $name;
+	private $summary;
+
+	// Ticket methods
+
+	// Ticket getters
+	function getType() {
+		return $type;
+	}
+
+	function getName() {
+		return $name;
+	}
+
+	function getSummary() {
+		return $summary;
+	}
+
+	// Ticket setters
+	function setType() {
+	;
+	}
+
+	function setName() {
+	;
+	}
+
+	function setSummary() {
+	;
+	}
+
+}
+
+?>
