7891c9fb6324db4c6d94bd3d0e843aeef0f288bf
Titou1384 <titou1384@gmail.com>
2014-09-19 20:36:22 +0200

    v0.0.3 : Implementing User object

---
+++ class/User.php
@@ -0,0 +1,40 @@
+<?php
+
+class User {
+
+	// Ticket attributes
+	private $gid;
+	private $username;
+	private $password; // Hashed password
+
+	// Ticket methods
+
+	// Ticket getters
+	function getGid() {
+		return $gid;
+	}
+
+	function getUsername() {
+		return $username;
+	}
+
+	function getPassword() {
+		return $password;
+	}
+
+	// Ticket setters
+	function setType() {
+	;
+	}
+
+	function setName() {
+	;
+	}
+
+	function setSummary() {
+	;
+	}
+
+}
+
+?>
